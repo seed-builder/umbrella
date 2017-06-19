06f0e74eb3e32722c754c5e5f16fb555e6ba2112
shinerain <529546542@qq.com>
2017-06-19 11:46:38 +0800

    资金流水

--- app/Http/Controllers/Admin/CustomerAccountRecordController.php
+++ app/Http/Controllers/Admin/CustomerAccountRecordController.php
@@ -1,74 +1,101 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
+use App\Models\CustomerAccount;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Admin\BaseController;
 use App\Models\CustomerAccountRecord;
 
 class CustomerAccountRecordController extends BaseController
 {
-	public function newEntity(array $attributes = [])
-	{
-		// TODO: Implement newEntity() method.
-		return new CustomerAccountRecord($attributes);
-	}
+    public function newEntity(array $attributes = [])
+    {
+        // TODO: Implement newEntity() method.
+        return new CustomerAccountRecord($attributes);
+    }
 
-	/**
-	* Display a listing of the resource.
-	*
-	* @return  \Illuminate\Http\Response
-	*/
-	public function index()
-	{
-		//
-		return view('admin.customer-account-record.index');
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('admin.customer-account-record.index');
+    }
 
-	/**
-	* Show the form for creating a new resource.
-	*
-	* @return  \Illuminate\Http\Response
-	*/
-	public function create()
-	{
-		return view('admin.customer-account-record.create');
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return  \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.customer-account-record.create');
+    }
 
-	/**
-	* Display the specified resource.
-	*
-	* @param    int  $id
-	* @return  \Illuminate\Http\Response
-	*/
-	public function edit($id)
-	{
-		$entity = CustomerAccountRecord::find($id);
-		return view('admin.customer-account-record.edit', ['entity' => $entity]);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param    int $id
+     * @return  \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $entity = CustomerAccountRecord::find($id);
+        return view('admin.customer-account-record.edit', ['entity' => $entity]);
+    }
 
-	/**
-	* Display the specified resource.
-	*
-	* @param    int  $id
-	* @return  \Illuminate\Http\Response
-	*/
-	public function show($id)
-	{
-		$entity = CustomerAccountRecord::find($id);
-		return view('admin.customer-account-record.show', ['entity' => $entity]);
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param    int $id
+     * @return  \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $entity = CustomerAccountRecord::find($id);
+        return view('admin.customer-account-record.show', ['entity' => $entity]);
+    }
 
-	/**
-	* @param  Request $request
-	* @param  array $searchCols
-	* @param  array $with
-	* @param  null $conditionCall
-	* @param  bool $all_columns
-	* @return  \Illuminate\Http\JsonResponse
-	*/
-	public function pagination(Request $request, $searchCols = [], $with=[], $conditionCall = null, $dataHandleCall = null, $all_columns = false){
-		$searchCols = ["remark"];
-		return parent::pagination($request, $searchCols);
-	}
+    /**
+     * @param  Request $request
+     * @param  array $searchCols
+     * @param  array $with
+     * @param  null $conditionCall
+     * @param  bool $all_columns
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function pagination(Request $request, $searchCols = [], $with = [], $conditionCall = null, $dataHandleCall = null, $all_columns = false)
+    {
+        $searchCols = ["remark"];
+        return parent::pagination($request, $searchCols, $with, $conditionCall, $dataHandleCall, true);
+    }
+
+    public function entityQuery()
+    {
+        $query = CustomerAccountRecord::query();
+        $query->select(
+            'customer_account_records.id as id',
+            'customer_account_records.amt',
+            'customer_account_records.type',
+            'customer_account_records.status',
+            'customer_account_records.remark',
+            'customer_account_records.created_at',
+            'customer_accounts.sn',
+            'customers.nickname'
+        );
+
+        $query->leftJoin('customers', function ($join) {
+            $join->on('customers.id', '=', 'customer_account_records.customer_id');
+        });
+
+        $query->leftJoin('customer_accounts', function ($join) {
+            $join->on('customer_accounts.id', '=', 'customer_account_records.customer_account_id');
+        });
+
+        return $query;
+    }
 
 }
--- resources/views/admin/customer-account-record/index.blade.php
+++ resources/views/admin/customer-account-record/index.blade.php
@@ -4,197 +4,145 @@
 @endsection
 
 @section('content')
-<div class="page-content">
-    <!-- BEGIN PAGE HEAD-->
-    <div class="page-head">
-        <!-- BEGIN PAGE TITLE -->
-        <div class="page-title">
-            <h1>top module
-                <small>customer_account_records</small>
-            </h1>
+    <div class="page-content">
+        <!-- BEGIN PAGE HEAD-->
+        <div class="page-head">
+            <!-- BEGIN PAGE TITLE -->
+            <div class="page-title">
+                <h1>top module
+                    <small>资金流水记录</small>
+                </h1>
+            </div>
+            <!-- END PAGE TITLE -->
         </div>
-        <!-- END PAGE TITLE -->
-    </div>
-    <!-- END PAGE HEAD-->
-    <!-- BEGIN PAGE BREADCRUMB -->
-    <ul class="page-breadcrumb breadcrumb">
-        <li>
-            <a href="/admin/">首页</a>
-            <i class="fa fa-circle"></i>
-        </li>
-        <li>
-            <span id="breadcrumb"  class="active">customer_account_records</span>
-        </li>
-    </ul>
-    <!-- END PAGE BREADCRUMB -->
-    <!-- BEGIN PAGE BASE CONTENT -->
-    <div class="row">
-        <div class="col-md-12">
-            <div class="portlet light bordered">
-                <div class="portlet-title">
-                    <div class="caption">
-                        <i class="fa fa-search"></i>
-                        <span class="caption-subject font-dark sbold uppercase">customer_account_records搜索</span>
+        <!-- END PAGE HEAD-->
+        <!-- BEGIN PAGE BREADCRUMB -->
+        <ul class="page-breadcrumb breadcrumb">
+            <li>
+                <a href="/admin/">首页</a>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <span id="breadcrumb" class="active">资金流水记录</span>
+            </li>
+        </ul>
+        <!-- END PAGE BREADCRUMB -->
+        <!-- BEGIN PAGE BASE CONTENT -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="portlet light bordered">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-search"></i>
+                            <span class="caption-subject font-dark sbold uppercase">资金流水记录搜索</span>
+                        </div>
+                        <div class="tools">
+                            <a href="" class="collapse"> </a>
+                        </div>
                     </div>
-                    <div class="tools">
-                        <a href="" class="collapse"> </a>
-                    </div>
-                </div>
-                <div class="portlet-body form">
-                    <form class="form-horizontal search-form">
-                        <div class="form-body">
-                                                            <div class="col-md-4">
+                    <div class="portlet-body form">
+                        <form class="form-horizontal search-form">
+                            <div class="form-body">
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="col-md-3 control-label">amt</label>
+                                        <label class="col-md-3 control-label">金额</label>
                                         <div class="col-md-9">
                                             <input type="text" class="form-control" name="filter[][amt]">
                                         </div>
                                     </div>
                                 </div>
-                                                            <div class="col-md-4">
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="col-md-3 control-label">created_at</label>
+                                        <label class="col-md-3 control-label">微信昵称</label>
                                         <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][created_at]">
+                                            <input type="text" class="form-control" name="filter[][nickname]">
                                         </div>
                                     </div>
                                 </div>
-                                                            <div class="col-md-4">
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="col-md-3 control-label">creator_id</label>
+                                        <label class="col-md-3 control-label">账户号</label>
                                         <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][creator_id]">
+                                            <input type="text" class="form-control"
+                                                   name="filter[][sn]">
                                         </div>
                                     </div>
                                 </div>
-                                                            <div class="col-md-4">
+
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="col-md-3 control-label">customer_account_id</label>
+                                        <label class="col-md-3 control-label">状态</label>
                                         <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][customer_account_id]">
+                                            <select class="form-control" name="filter[][status]">
+                                                <option value="">请选择</option>
+                                                <option value="1">未完成</option>
+                                                <option value="2">已完成</option>
+                                                <option value="3">取消</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
-                                                            <div class="col-md-4">
+                                <div class="col-md-4">
                                     <div class="form-group">
-                                        <label class="col-md-3 control-label">customer_id</label>
+                                        <label class="col-md-3 control-label">类别</label>
                                         <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][customer_id]">
+                                            <select class="form-control" name="filter[][type]">
+                                                <option value="">请选择</option>
+                                                <option value="1">充值</option>
+                                                <option value="2">支出</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">deleted_at</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][deleted_at]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">id</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][id]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">modifier_id</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][modifier_id]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">remark</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][remark]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">status</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][status]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">type</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][type]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                            <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label class="col-md-3 control-label">updated_at</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" name="filter[][updated_at]">
-                                        </div>
-                                    </div>
-                                </div>
-                                                    </div>
-                        <div class="row"></div>
+                            </div>
+                            <div class="row"></div>
 
 
-                        <div class="form-actions right">
-                            <button type="button" class="btn green table-search">查询</button>
-                            <button type="button" class="btn red table-reset">重置</button>
+                            <div class="form-actions right">
+                                <button type="button" class="btn green table-search">查询</button>
+                                <button type="button" class="btn red table-reset">重置</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="portlet light bordered">
+                    <div id="alert-id"></div>
+
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="fa fa-th-list"></i>
+                            <span class="caption-subject bold uppercase font-dark">资金流水记录</span>
                         </div>
-
-                    </form>
+                        <div class="tools">
+                            <a href="" class="collapse"> </a>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
+                        <table id="moduleTable" class="table table-bordered table-hover display nowrap" cellspacing="0"
+                               width="100%">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>微信昵称</th>
+                                <th>账户号</th>
+                                <th>金额</th>
+                                <th>类型</th>
+                                <th>状态</th>
+                                <th>创建时间</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <div class="portlet light bordered">
-                <div id="alert-id"></div>
-
-                <div class="portlet-title">
-                    <div class="caption">
-                        <i class="fa fa-th-list"></i>
-                        <span class="caption-subject bold uppercase font-dark">customer_account_records</span>
-                    </div>
-                    <div class="tools">
-                        <a href="" class="collapse"> </a>
-                    </div>
-                </div>
-                <div class="portlet-body">
-                    <table id="moduleTable" class="table table-bordered table-hover display nowrap" cellspacing="0" width="100%">
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th width="10%">操作</th>
-                                                            <th>amt</th>
-                                                            <th>created_at</th>
-                                                            <th>creator_id</th>
-                                                            <th>customer_account_id</th>
-                                                            <th>customer_id</th>
-                                                            <th>deleted_at</th>
-                                                            <th>id</th>
-                                                            <th>modifier_id</th>
-                                                            <th>remark</th>
-                                                            <th>status</th>
-                                                            <th>type</th>
-                                                            <th>updated_at</th>
-                            
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
         <!-- END PAGE BASE CONTENT -->
-</div>
+    </div>
 
 
 @endsection
@@ -203,7 +151,7 @@
     <script type="text/javascript">
         $(function () {
             seajs.use('admin/customer_account_record.js', function (app) {
-                app.index($, 'moduleTable','alert-id');
+                app.index($, 'moduleTable', 'alert-id');
             });
         });
     </script>
