770da7fec92a42bc91696c4e5b3bfeabac067d61
shinerain <529546542@qq.com>
2017-07-02 20:46:47 +0800

    微信端 资金流水详情

--- public/assets/mobile/customer_account_record.js
+++ public/assets/mobile/customer_account_record.js
@@ -19,20 +19,14 @@ define(function (require, exports, module) {
                         return '/mobile/customer-account-record/view/' + item.id
                     },
                     left: function (data) {
-                        if (data.type == 1)
-                            return '账户充值'
-                        else if (data.type == 2)
-                            return '押金支出'
-                        else if (data.type == 3)
-                            return '租金支出'
-                        else if (data.type == 4)
-                            return '押金退回'
+                        return data.type_name;
                     },
                     right: function (data) {
-                        if (data.type == 2 || data.type == 3)
-                            return '<span class="amt-out">' + data.amt + '</span>'
-                        else
-                            return '<span class="amt-in">' + data.amt + '</span>'
+                        return data.amt
+                        // if (data.type == 2 || data.type == 3)
+                        //     return '<span class="amt-out">' + data.amt + '</span>'
+                        // else
+                        //     return '<span class="amt-in">' + data.amt + '</span>'
                     },
                     footer: function (data) {
                         return data.created_at
