bdcee9bca3d1de9ab0ca951291fb66e4a17fdf37
gsjohn <sz.gs.john@gmail.com>
2017-07-06 22:41:34 +0800

    new functions

--- database/seeds/EquipmentSeeder.php
+++ database/seeds/EquipmentSeeder.php
@@ -19,7 +19,7 @@ class EquipmentSeeder extends Seeder
         for ($i=0;$i<100;$i++){
             $type = $faker->randomElement([1,2]);
             $data[] = [
-                'sn' => $type==1 ? 'E'.$faker->date('YmdHis').$faker->lexify('???') : 'M'.$faker->date('YmdHis').$faker->lexify('???'),
+                'sn' => $type==1 ? 'E'.$faker->date('Ym').$faker->lexify('????') : 'M'.$faker->date('Ym').$faker->lexify('????'),
                 'site_id' => $faker->randomElement($site_ids),
                 'capacity' => $faker->randomElement([50,100,150,200]),
                 'have' => $faker->randomNumber($nbDigits = NULL),
