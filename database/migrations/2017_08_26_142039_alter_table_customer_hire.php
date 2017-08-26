04f47ac98ba5923185162d58f8d614b11d00b06f
gsjohn <sz.gs.john@gmail.com>
2017-08-26 14:27:24 +0800

    new functions

---
+++ database/migrations/2017_08_26_142039_alter_table_customer_hire.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableCustomerHire extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer_hires', function (Blueprint $table) {
+            //
+	        $table->integer("price_id")->default(0)->comment('价格id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer_hires', function (Blueprint $table) {
+            //
+	        $table->dropColumn('price_id');
+        });
+    }
+}
