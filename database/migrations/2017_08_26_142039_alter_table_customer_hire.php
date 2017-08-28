47f37b421a53e48ff16a89d72f5c772e7c4570ca
gsjohn <sz.gs.john@gmail.com>
2017-08-28 09:29:06 +0800

    new functions

--- database/migrations/2017_08_26_142039_alter_table_customer_hire.php
+++ database/migrations/2017_08_26_142039_alter_table_customer_hire.php
@@ -16,7 +16,7 @@ class AlterTableCustomerHire extends Migration
         Schema::table('customer_hires', function (Blueprint $table) {
             //
 	        $table->integer("price_id")->default(0)->comment('价格id');
-	        $table->decimal("hire_day")->default(0)->comment('租借天数')->change();
+	        $table->decimal("hire_day")->change();
         });
     }
 
