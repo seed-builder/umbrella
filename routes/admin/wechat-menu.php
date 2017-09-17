fb6cc54b97a8aa45d711781b0a4321a6e76a0836
shinerain <529546542@qq.com>
2017-09-17 17:06:26 +0800

    bug fix

--- routes/admin/wechat-menu.php
+++ routes/admin/wechat-menu.php
@@ -9,5 +9,6 @@
 
 Route::get('wechat-menu/index', ['uses' => 'WechatMenuController@index']);
 Route::any('wechat-menu/store', ['uses' => 'WechatMenuController@store']);
+Route::any('wechat-menu/pro-store', ['uses' => 'WechatMenuController@proStore']);
 Route::get('wechat-menu/delete', ['uses' => 'WechatMenuController@delete']);
 Route::post('wechat-menu/update', ['uses' => 'WechatMenuController@update']);
