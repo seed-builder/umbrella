0458ca9490c3cd156c8ca21759adbfb1d2fef504
shinerain <529546542@qq.com>
2017-09-21 14:26:50 +0800

    bug fix

--- app/Http/Controllers/Mobile/HomeController.php
+++ app/Http/Controllers/Mobile/HomeController.php
@@ -29,6 +29,10 @@ class HomeController extends MobileController
     public function register(Request $request){
         $phone = $request->input('mobile');
         $code = $request->input('code');
+
+        if (empty($code))
+            return $this->fail_result('请输入验证码！');
+
         $resp = Dysms::checkVerifyCode($phone, $code);
 
         if (!$resp)
