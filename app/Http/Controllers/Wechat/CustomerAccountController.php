05642fdad682baa72c20c088c91287d70fb0f863
shinerain <529546542@qq.com>
2017-10-03 00:56:42 +0800

    wechat

--- app/Http/Controllers/Wechat/CustomerAccountController.php
+++ app/Http/Controllers/Wechat/CustomerAccountController.php
@@ -20,8 +20,8 @@ class CustomerAccountController extends BaseController
 	public function get(){
 	    $customer = $this->request->cutomer;
 
-//	    $account = CustomerAccount::where('customer_id',$customer->id)->first();
-	    $account = CustomerAccount::where('customer_id',78)->first();
+	    $account = CustomerAccount::where('customer_id',$customer->id)->first();
+//	    $account = CustomerAccount::where('customer_id',78)->first();
         $account->total_deposit = number_format($account->deposit + $account->freeze_deposit,2);
 
 	    return $this->success_result('',$account);
