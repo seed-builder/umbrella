a13f1f5d7410810f44b8cc849d62804b359b1ae4
gsjohn <sz.gs.john@gmail.com>
2017-10-03 22:24:07 +0800

    bug fix

--- resources/templates/api/controller.blade.php
+++ resources/templates/api/controller.blade.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ApiController;
-use App\Models\Busi\{{$model}};
+use App\Models\{{$model}};
 
 class {{$model}}Controller extends ApiController
 {
