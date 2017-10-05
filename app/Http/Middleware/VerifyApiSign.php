8250dede1214fa1340bdf8d59c2173e48f939c63
gsjohn <sz.gs.john@gmail.com>
2017-10-05 23:10:05 +0800

    bug fix

--- app/Http/Middleware/VerifyApiSign.php
+++ app/Http/Middleware/VerifyApiSign.php
@@ -29,20 +29,20 @@ class VerifyApiSign
         }
 
         $token = $request->input('token', '');
-//        if (empty($token))
-//            return $this->fail(401, '令牌不能为空');
-//
-//        if (!Cache::has($token))
-//            return $this->fail(403, '令牌不存在或者已失效');
+        if (empty($token))
+            return $this->fail(401, '令牌不能为空');
 
-//        if (!empty($token)) {
-//            $customer_id = Cache::get($token);
-//            if(!empty($customer_id) && $customer_id > 0)
-//            {
-//                $request->customer = Customer::find($customer_id);
-//            }
-//        }
-        $request->customer = Customer::find(85);
+        if (!Cache::has($token))
+            return $this->fail(403, '令牌不存在或者已失效');
+
+        if (!empty($token)) {
+            $customer_id = Cache::get($token);
+            if(!empty($customer_id) && $customer_id > 0)
+            {
+                $request->customer = Customer::find($customer_id);
+            }
+        }
+//        $request->customer = Customer::find(85);
         return $next($request);
     }
 
